857984c05a5c06e2be23c78d95d0edd7c662ec5e
Gregg <gjmos94@outlook.com>
2021-09-11 04:01:47 -0500

    Initial Commit

---
+++ GUI.py
@@ -0,0 +1,74 @@
+#test comment
+import tkinter as tk
+import pandas as pd
+import numpy as np
+from PIL import Image, ImageTk
+from tkinter.filedialog import askopenfile
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, width=600, height=300)
+canvas.grid(columnspan=3)
+
+#logo
+logo= Image.open('iland.png')
+logo = ImageTk.PhotoImage(logo)
+logo_label = tk.Label(image=logo)
+logo_label.image = logo
+logo_label.grid(column=1, row=0)
+
+#open file function
+def open_file():
+    browse_text.set("Loading...")
+    file = askopenfile(parent=root, mode='rb', title="choose a file")
+    clean_rev(file)
+
+def clean_rev(x, m1, m2, y):
+    # This loads the CSV file into the console
+    df1 = pd.read_csv(x)
+    df1.columns = [
+        'Posted_Dt',
+        'Doc_Dt',
+        'Doc',
+        'Memo / Description',
+        'Department',
+        'Location',
+        'Contract',
+        'Customer Name',
+        'JNL',
+        'Curr',
+        'Txn Amt',
+        'Debit',
+        'Credit',
+        'Balance (USD)'
+    ]
+    df1 = df1.fillna(0)
+
+    df1["Total Billed"] = df1.Credit - df1.Debit
+
+    df1.drop(df1[df1['Memo / Description'] == 0].index, inplace=True)
+
+    df1['Posted_Dt'] = pd.DatetimeIndex(df1['Posted_Dt']).month
+
+    pivot1 = pd.pivot_table(df1, index=['Contract', 'Customer Name'],
+                                columns='Posted_Dt',
+                                values='Total Billed',
+                                aggfunc='sum')
+
+    df2 = pd.DataFrame(pivot1.to_records())
+
+    df2 = df2.fillna(0)
+
+    df2["Variance"] = df2[m1] - df2[m2]
+
+    df_final = df2[(df2.Variance >= y) | (df2.Variance <= -y)]
+    df_final.to_csv(r'C:\Users\mosco\OneDrive\Documents\Python Scripts\final_rev_analysis.csv')
+
+#Browse Button Code
+browse_text = tk.StringVar()
+browsebtn = tk.Button(root, textvariable=browse_text, command=lambda:open_file(), font="raleway")
+browse_text.set("Browse")
+browsebtn.grid(column=1, row=2)
+
+root.mainloop()
+
