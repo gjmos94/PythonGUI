6c36a0bc665fa9b77371c37f7bd4293605d3aea2
Gregg <gjmos94@outlook.com>
2021-09-22 23:11:43 -0500

    Completed functionality for the first Function with GUI and entry boxes

--- GUI.py
+++ GUI.py
@@ -11,17 +11,22 @@ from tkinter.filedialog import askopenfile
 def open_file():
     x1 = " "
     browse_text.set("Loading...")
-    file = askopenfile(parent=root, mode='r', title="choose a file", filetype=[("Excel file", "*.xlsx")])
-    #file.write("Hello World$!!!")
-    browse_text.set("Processing..")
-    x1 = file.name
-    print(x1)  #This is printing out the exact address of chosen file
-    clean_rev(x1, e1, e2, e3)
+    file = askopenfile(parent=root, mode='r', title="choose a file", filetype=[("CSV File", "*.csv")])
+    x1 = file.name  # This is getting the exact file address
+    eStr1 = e1.get()   # These are getting the inputs from Entry boxes 1-3
+    eStr2 = e2.get()
+    eStr3 = int(e3.get())
 
+    clean_rev(x1, eStr1, eStr2, eStr3) #This line tests Entry input
+    browse_text.set("Run")
 def callClean():
-    x1 = ''
     open_file()
-   # clean_rev(x1, e1, e2, e3)
+
+def testInput(f1,a1,a2,a3): # to test openfile and entry box input
+    print(type(f1))
+    print(type(a1))
+    print(type(a2))
+    print(type(int(a3)))
 
 def clean_rev(x, m1, m2, y):
     # This loads the CSV file into the console
@@ -62,7 +67,7 @@ def clean_rev(x, m1, m2, y):
     df2["Variance"] = df2[m1] - df2[m2]
 
     df_final = df2[(df2.Variance >= y) | (df2.Variance <= -y)]
-    df_final.to_csv(r'C:\Users\mosco\OneDrive\Documents\Python Scripts\final_rev_analysis.csv')
+    df_final.to_csv(r'C:\Users\gregi\Downloads\finalTest.csv')
 
 def clicked(value):
     myLabel = tk.Label(root, text=value)
@@ -110,44 +115,27 @@ instructions.grid(column=5, row=1)
     df_final.to_csv(r'C:\Users\mosco\OneDrive\Documents\Python Scripts\final_rev_analysis.csv')
 
 #input boxes
-tk.Label(root, text="Value 1").grid(column=1, row=0,padx=20)
-tk.Label(root, text="Value 2").grid(column=1, row=1,padx=20)
-tk.Label(root, text="Value 3").grid(column=1, row=2,padx=20)
+tk.Label(root, text="Value 1").grid(column=1, row=0, padx=20)
+tk.Label(root, text="Value 2").grid(column=1, row=1, padx=20)
+tk.Label(root, text="Value 3").grid(column=1, row=2, padx=20)
 
 e1 = tk.Entry(root)
 e2 = tk.Entry(root)
 e3 = tk.Entry(root)
-#e4 = tk.Entry(root)
+
+
 
 e1.grid(row=0, column=2)
 e2.grid(row=1, column=2)
 e3.grid(row=2, column=2)
-#e4.grid(row=3, column=2)
-#Browse Button Code
 
 
-browse_text = tk.StringVar()                                                         #changed font, color, and bg of button
-browsebtn = tk.Button(root, textvariable=browse_text, command=callClean, font="helvetica 12 bold", bg="navy blue", fg="gold", height=1, width=15)
+
+browse_text = tk.StringVar()                                                         # changed font, color, and bg of button
+browsebtn = tk.Button(root, textvariable=browse_text, command=open_file, font="helvetica 12 bold", bg="navy blue", fg="gold", height=1, width=15)
 browse_text.set("Run")
 browsebtn.grid(column=5, row=2)
-#button 2
-#browse_text2 = tk.StringVar()
-#browsebtn2 = tk.Button(root, textvariable=browse_text2, command=open_file, font="helvetica 12 bold", bg="navy blue", fg="gold", height=1 , width=15)
-#browse_text2.set("Sales Register")
-#browsebtn2.grid(column=6, row=4)
-#button 3
-#browse_text3 = tk.StringVar()
-#browsebtn3 = tk.Button(root, textvariable=browse_text3, command=open_file, font="helvetica 12 bold", bg="navy blue", fg="gold", height=1 , width=15)
-#browse_text3.set("Burst Rate Check")
-#browsebtn3.grid(column=6, row=6)
 
-#browse_text4 = tk.StringVar()
-#browsebtn4 = tk.Button(root, textvariable=browse_text4, command=open_file, font="helvetica 12 bold", bg="navy blue", fg="gold", height=1 , width=15)
-#browse_text4.set("Choose a File")
-#browsebtn4.grid(column=6, row=8)
-#changed Canvas and button color
-#canvas.configure(background='white')
-#root.configure(background="white")
 
 
 root.mainloop()
