ce5f72d79612b8a86b407b887e24ef11719feb57
Gregg <gjmos94@outlook.com>
2021-09-24 23:09:16 -0500

    Added frame for second menu and window text output for CSV file name being processed

--- GUI.py
+++ GUI.py
@@ -17,7 +17,14 @@ def open_file():
     eStr2 = e2.get()
     eStr3 = int(e3.get())
 
-    clean_rev(x1, eStr1, eStr2, eStr3) #This line tests Entry input
+    clean_rev(x1, eStr1, eStr2, eStr3)
+    newWindow = tk.Toplevel(root)
+    newWindow.geometry("300x50")
+    completeLabel = tk.Label(newWindow, text=file.name)
+    completeLabel.grid(column=3, row=4)
+    completeLabel2 = tk.Label(newWindow, text="has been processed")
+    completeLabel2.grid(column=4, row=4)
+
     browse_text.set("Run")
 def callClean():
     open_file()
@@ -70,8 +77,31 @@ def clean_rev(x, m1, m2, y):
     df_final.to_csv(r'C:\Users\gregi\Downloads\finalTest.csv')
 
 def clicked(value):
-    myLabel = tk.Label(root, text=value)
-    myLabel.pack
+    if value == 2:
+        # Added frames with new radio buttons and entry boxes for secondary apps
+       center = tk.Frame(root, bg="teal", width=290, height=145)
+       center.place(width=600, height=250)
+       lab1=tk.Label(center, text="Value 1").grid(column=1, row=0, padx=20)
+       lab2=tk.Label(center, text="Value 2").grid(column=1, row=1, padx=20)
+       lab3=tk.Label(center, text="Value 3").grid(column=1, row=2, padx=20)
+       e1 = tk.Entry(center)
+       e2 = tk.Entry(center)
+       e3 = tk.Entry(center)
+       e1.grid(row=0, column=2)
+       e2.grid(row=1, column=2)
+       e3.grid(row=2, column=2)
+       r = tk.IntVar()
+       r.set("2")
+
+       radB2 = tk.Radiobutton(root, text="Revenue Clean-up2", variable=r, value=1, command=lambda: clicked(r.get()))
+       radB2.grid(column=1, row=3)
+       radB2 = tk.Radiobutton(root, text="Sales Register2", variable=r, value=2, command=lambda: clicked(r.get()))
+       radB2.grid(column=2, row=3)
+
+
+
+
+
 ##########################################################################################################
 # $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
 ##########################################################################################################
@@ -88,31 +118,16 @@ logo_label.grid(column=5, row=0, padx=50)
 
 # radio buttons
 r = tk.IntVar()
-r.set("5")
+r.set("1")
 
 radB=tk.Radiobutton(root, text="Revenue Clean-up", variable=r, value=1, command=lambda: clicked(r.get()))
 radB.grid( column=5, row=3)
 radB=tk.Radiobutton(root, text="Sales Register", variable=r, value=2, command=lambda: clicked(r.get()))
 radB.grid( column=2, row=3)
-radB=tk.Radiobutton(root, text="Burst rate check", variable=r, value=3, command=lambda: clicked(r.get()))
-radB.grid( column=1, row=3, padx=50)
+
 # instructions
 instructions= tk.Label(root, text="Select a file to process", font="helvetica 12 bold", bg="white")
 instructions.grid(column=5, row=1)
-
-    pivot1 = pd.pivot_table(df1, index=['Contract', 'Customer Name'],
-                                columns='Posted_Dt',
-                                values='Total Billed',
-                                aggfunc='sum')
-
-    df2 = pd.DataFrame(pivot1.to_records())
-
-    df2 = df2.fillna(0)
-
-    df2["Variance"] = df2[m1] - df2[m2]
-
-    df_final = df2[(df2.Variance >= y) | (df2.Variance <= -y)]
-    df_final.to_csv(r'C:\Users\mosco\OneDrive\Documents\Python Scripts\final_rev_analysis.csv')
 
 #input boxes
 tk.Label(root, text="Value 1").grid(column=1, row=0, padx=20)
@@ -139,6 +154,7 @@ browsebtn.grid(column=5, row=2)
 
 
 root.mainloop()
+
 
 
 
